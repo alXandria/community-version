3e62c56ab95150af7552bff1bbb32f89083ccf38
Joel Benson <joel.benson@wustl.edu>
2022-08-25 01:16:08 -0400

    AlXandria UI

---
+++ alxandria/components/layout.js
@@ -0,0 +1,33 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
+export const siteTitle = 'AlXandria';
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="The Decentralized Encyclopedia"
+          content="Decentralizing the production of the sum of human knowledge"
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
+      <main>{children}</main>
+      
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ alxandria/pages/documents/encyclopedia.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/layout';
+
+export default function Encyclopedia() {
+    
+    return (
+        <Layout>
+          <Head>
+            <title>Encyclopedia</title>
+          </Head>
+
+          <h1>Encyclopedia</h1>
+
+          <p>
+            An <b>encyclopedia</b> (<a href="https://en.wikipedia.org/wiki/American_English">American English</a>), or <b>encyclopaedia</b> (<a href="https://en.wikipedia.org/wiki/British_English">British English</a>) is a <a href="https://en.wikipedia.org/wiki/Reference_work">reference work</a> or <a href="https://en.wikipedia.org/wiki/Compendium">compendium</a> providing summaries of <a href="https://en.wikipedia.org/wiki/Knowledge">knowledge</a> either general or special to a particular field or discipline.[1] Encyclopedias are divided into <a href="https://en.wikipedia.org/wiki/Article_(publishing)">articles</a> or entries that are arranged <a href="https://en.wikipedia.org/wiki/Alphabetical_order">alphabetically</a> by article name[2] or by thematic categories, or else are <a href="https://en.wikipedia.org/wiki/Hyperlink">hyperlinked</a> and searchable by <a href="https://en.wikipedia.org/wiki/Random_access">random access</a>. Encyclopedia entries are longer and more detailed than those in most <a href="https://en.wikipedia.org/wiki/Dictionary">dictionaries</a>.[2] Generally speaking, encyclopedia articles focus on <a href="https://en.wikipedia.org/wiki/Fact">factual</a> information concerning the subject named in the article's title; this is unlike dictionary entries, which focus on <a href="https://en.wikipedia.org/wiki/Linguistics">linguistic</a> information about <a href="https://en.wikipedia.org/wiki/Word">words</a>, such as their <a href="https://en.wikipedia.org/wiki/Etymology">etymology</a>, meaning, <a href="https://en.wikipedia.org/wiki/Pronunciation">pronunciation</a>, use, and <a href="https://en.wikipedia.org/wiki/Grammar">grammatical</a> forms.[3][4][5][6]
+          </p>
+
+          <p>
+            Encyclopedias have existed for around 2,000 years and have evolved considerably during that time as regards language (written in a major international or a vernacular language), size (few or many volumes), intent (presentation of a global or a limited range of knowledge), cultural perspective (authoritative, ideological, didactic, utilitarian), authorship (qualifications, style), readership (education level, background, interests, capabilities), and the technologies available for their production and distribution (hand-written manuscripts, small or large print runs, Internet). As a valued source of reliable information compiled by experts, printed versions found a prominent place in libraries, schools and other educational institutions.
+          </p>
+        
+          <p>
+            The appearance of <a href="https://en.wikipedia.org/wiki/Online_encyclopedia">digital and open-source versions</a> in the 21st century, such as <a href="https://en.wikipedia.org/wiki/Wikipedia">Wikipedia</a> <em>and <a href="/documents/alxandria">AlXandria</a></em>, has vastly expanded the accessibility, authorship, readership, and variety of encyclopedia entries.[7]
+          </p>
+
+        </Layout>
+      );
+
+}
