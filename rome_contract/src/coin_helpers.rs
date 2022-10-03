897a10dfe39027793f4c50ce995220056a7fc91f
tacolopo <106569305+tacolopo@users.noreply.github.com>
2022-10-03 12:49:03 -0400

    remove comments

--- rome_contract/src/coin_helpers.rs
+++ rome_contract/src/coin_helpers.rs
@@ -1,12 +1,6 @@
 use crate::error::ContractError;
 use cosmwasm_std::Coin;
 
-// Modified:
-// https://github.com/InterWasm/cw-contracts/blob/main/contracts/nameservice/src/contract.rs
-// just instead of
-// coin.amount.u128() >= required_amount
-// it is `coin.amount.u128() == required_amount`
-
 pub fn assert_sent_exact_coin(sent: &[Coin], required: Option<Coin>) -> Result<(), ContractError> {
     if let Some(required_coin) = required {
         let required_amount = required_coin.amount.u128();
